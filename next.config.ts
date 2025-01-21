90a96317296bd0cefa66fc12ff6ccf7883a14760
Son Ahyun <dkgus731@naver.com>
2025-01-21 14:00:12 +0900

    💄 프로젝트 레이아웃 및 스타일 설정 추가
    - next.config.ts 수정: SCSS 프리프로세서 설정 추가
    - globals.css를 globals.scss로 변경 및 리팩터링
    - 레이아웃 구조를 관리하는 layout.tsx 파일 수정
    - 페이지 스타일을 관리하는 page.module.scss 파일 추가
    - _mixins.scss와 _variables.scss 파일 생성: 공통 스타일 변수 및 믹스인 정의
    - package.json 및 package-lock.json 수정: 관련 패키지 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import withPWA from "next-pwa";
+import withPWA from 'next-pwa';
+import path from 'path';
 
 const nextPWA = withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development", // 개발 환경에서 비활성화
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // 개발 환경에서 비활성화
 });
 
 const nextConfig = {
@@ -10,6 +11,10 @@ const nextConfig = {
   webpack: (config: { cache: boolean }) => {
     config.cache = false; // Webpack 캐시 비활성화
     return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "_variables.scss"; @import "_mixins.scss";`,
   },
 };
 
