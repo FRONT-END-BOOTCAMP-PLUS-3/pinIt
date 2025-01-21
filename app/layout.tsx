b2e0ba1c97e87390c40bcd53dc47e7a6d6326d52
Son Ahyun <dkgus731@naver.com>
2025-01-21 15:33:05 +0900

    💄 모바일 뷰로 보여지도록 전체 레이아웃 설정
    - 모바일 퍼스트 디자인 적용
    - 컨테이너 가로 폭 및 패딩 설정
    - 반응형 레이아웃 지원 추가
    - 글로벌 스타일 및 변수 정리
    - PWA 환경 설정 및 manifest.json 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,4 @@
 import './globals.scss';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({
-  subsets: ['latin'],
-});
-
-export const metadata: Metadata = {
-  title: 'PinIt!', // 고정 타이틀
-  description: 'pin_your_spot.', // 기본 설명
-};
 
 export default function RootLayout({
   children,
@@ -17,11 +6,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='ko'>
       <head>
         <link rel='manifest' href='/manifest.json' />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className='container'>{children}</div>
+      </body>
     </html>
   );
 }
