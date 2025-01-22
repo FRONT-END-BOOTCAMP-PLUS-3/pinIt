2428d7e925577a57cb9ec90f32828c9a921c662e
Son Ahyun <dkgus731@naver.com>
2025-01-22 11:20:47 +0900

    🔨 본문이 header에 가려지지 않도록 마진 추가 설정

--- app/(anon)/page.tsx
+++ app/(anon)/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>ㅎㅇ안녕하세요!! 이거 프리텐다드 맞나</div>;
+  return (
+    <>
+      <div style={{ margin: '60px' }}>
+        ㅎㅇ안녕하세요!! 이거 프리텐다드 맞나
+      </div>
+    </>
+  );
 }
