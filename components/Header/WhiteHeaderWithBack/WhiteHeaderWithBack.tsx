34f4df107ce1e0936ddacb6e680a75c76703a369
Son Ahyun <dkgus731@naver.com>
2025-01-22 12:17:50 +0900

    ✨ (#7) WhiteHeaderWithBack 컴포넌트 개발 및 관련 스타일 추가
    - WhiteHeaderWithBack 컴포넌트 생성
      - 헤더에 뒤로가기 아이콘 추가
      - 뒤로가기 아이콘 클릭시 이전 페이지로 이동
      - 스타일 적용 및 스크롤 시 동작 구현
    - WhiteHeaderWithBack 관련 SCSS 파일 추가 및 수정
      - Header.module.scss 파일 수정
      - WhiteHeader.module.scss 파일 추가

---
+++ components/Header/WhiteHeaderWithBack/WhiteHeaderWithBack.tsx
@@ -0,0 +1,30 @@
+'use client'; // React Client Component
+
+import { useRouter } from 'next/navigation'; // useRouter 훅 사용
+import styles from '../WhiteHeader.module.scss'; // SCSS 파일 불러오기
+import useScrollVisibility from '@/hooks/useScrollVisibility'; // 커스텀 훅 불러오기
+import Icon from '@/components/Icon/Icon';
+
+const WhiteHeaderWithBack = () => {
+  const isVisible = useScrollVisibility(); // 스크롤 가시성 상태 가져오기
+  const router = useRouter(); // Next.js 라우터 훅 가져오기
+
+  const handleBack = () => {
+    router.back(); // 이전 페이지로 이동
+  };
+
+  return (
+    <header className={`${styles.header} ${!isVisible ? styles.hidden : ''}`}>
+      <div
+        className={styles.icon}
+        onClick={handleBack}
+        role='button'
+        tabIndex={0}
+      >
+        <Icon id='left-black' />
+      </div>
+    </header>
+  );
+};
+
+export default WhiteHeaderWithBack;
