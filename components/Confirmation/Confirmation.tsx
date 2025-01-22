d2247a384ec473bc770be49958cdd0777ac93e31
seungsu-K <kss095@naver.com>
2025-01-23 01:01:02 +0900

    ✨ (#8) Confirmation 컴포넌트
    - props (text, opened, ref, onClickConfirmation, modalClse)
    - 팝업창 외부 클릭 시 모달 닫기 (useOnClickOutsideModal 훅)

--- components/Confirmation/Confirmation.tsx
+++ components/Confirmation/Confirmation.tsx
@@ -1,15 +1,35 @@
 import style from '@/components/Confirmation/Confirmation.module.scss';
+import Button from '../Buttons/Button';
+import { MouseEventHandler } from 'react';
 
-const Confirmation = ({ text }: { text: string }) => {
+const Confirmation = ({
+  text,
+  opened,
+  ref,
+  onClickConfirmation,
+  modalClose,
+}: {
+  text: string;
+  opened: boolean;
+  ref: React.RefObject<HTMLDivElement | null>;
+  onClickConfirmation: MouseEventHandler;
+  modalClose: MouseEventHandler;
+}) => {
   return (
-    <div className={style.confirmation}>
-      <div className={style.confirmation_contents}>
-        <div>{text}</div>
-        <div className={style.button_container}>
-          <button type='button'>확인</button>
-          <button type='button'>취소</button>
+    <div
+      className={
+        opened ? `${style.confirmation} ${style.open}` : style.confirmation
+      }
+    >
+      {opened ? (
+        <div className={style.contents} ref={ref}>
+          <p>{text}</p>
+          <div className={style.button_container}>
+            <Button label='확인' onClickButton={onClickConfirmation}></Button>
+            <Button label='취소' onClickButton={modalClose}></Button>
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
