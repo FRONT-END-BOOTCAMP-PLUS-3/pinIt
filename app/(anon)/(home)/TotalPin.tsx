5fb82f5d9cab0c49e37039de48f4edec3cec03d3
Jin <jin62413@gmail.com>
2025-01-24 00:36:49 +0900

    ✨ (#20) 전체 핀 레이아웃 추가
    - 핀 불러오기
    - 스타일 적용

---
+++ app/(anon)/(home)/TotalPin.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import PinCard from '@/components/Card/PinCard/PinCard';
+import styles from './ThisWeekChallenge.module.scss';
+
+const TotalPin = () => {
+  const imageData = [
+    {
+      id: 1,
+      title: 'Image 1',
+      imageUrl: '/headerLogo.png',
+    },
+    { id: 2, title: 'Image 2', imageUrl: '/headerLogo.png' },
+    { id: 3, title: 'Image 3', imageUrl: '/headerLogo.png' },
+    { id: 4, title: 'Image 4', imageUrl: '/headerLogo.png' },
+    { id: 5, title: 'Image 5', imageUrl: '/headerLogo.png' },
+  ];
+
+  const handleClick = () => {
+    console.log('h');
+  };
+
+  return (
+    <>
+      <div>
+        <h2 className={styles.title}>전체 핀</h2>
+        <div className={styles.pinCardContainer}>
+          {imageData.map((img) => {
+            return (
+              <PinCard
+                key={img.id}
+                alt={img.title}
+                url={img.imageUrl}
+                location={'남산타워'}
+                address={'서울 용산구'}
+                liked={false}
+                onClickLikeButton={handleClick}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TotalPin;
