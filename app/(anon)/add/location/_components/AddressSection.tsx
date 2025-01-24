96c240ca255cefacc52533d8bb5ff949a997dfb3
Son Ahyun <dkgus731@naver.com>
2025-01-24 10:33:23 +0900

    ✨ (#21) AddLocationPage를 2개의 컴포넌트로 분리, 스타일 파일 생성
    - `addLocation.module.scss` 파일 생성
    - `AddLocationPage`를 `MapSection`, `AddressSection`으로 분리

---
+++ app/(anon)/add/location/_components/AddressSection.tsx
@@ -0,0 +1,6 @@
+import styles from '../addLocation.module.scss';
+const AddressSection: React.FC = () => {
+  return <div className={styles.addressSectionContainer}>AddressSection</div>;
+};
+
+export default AddressSection;
---
+++ app/(anon)/add/location/_components/MapSection.tsx
@@ -0,0 +1,7 @@
+import styles from '../addLocation.module.scss';
+
+const MapSectoion: React.FC = () => {
+  return <div className={styles.mapSectionContainer}>MapSectoion</div>;
+};
+
+export default MapSectoion;
