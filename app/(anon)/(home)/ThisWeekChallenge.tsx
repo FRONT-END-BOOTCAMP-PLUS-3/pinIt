0112ca226a8196bb74670e0ddf1808f68122ae6b
jin62413 <jin62413@gmail.com>
2025-01-24 17:40:38 +0900

    🔨 (#20) 이번주 챌린지 레이아웃 수정
    - 더보기 버튼 추가
    - 이미지 카드 링크로 변환

--- app/(anon)/(home)/ThisWeekChallenge.tsx
+++ app/(anon)/(home)/ThisWeekChallenge.tsx
@@ -3,8 +3,11 @@
 import RoundIconButton from '@/components/Buttons/RoundIconButton';
 import styles from './ThisWeekChallenge.module.scss';
 import ImageCard from '@/components/Card/ImageCard/ImageCard';
+import Link from 'next/link';
+import ROUTES from '@/constants/routes';
 
 const ThisWeekChallenge = () => {
+  const pinIdData = 'pin-id';
   const imageData = [
     {
       id: 1,
@@ -23,16 +26,30 @@ const ThisWeekChallenge = () => {
   return (
     <>
       <div className={styles.thisWeekChallengeContainer}>
-        <h2 className={styles.title}>이번주 포토챌린지</h2>
-        <div className={styles.imageCardContainer}>
-          {imageData.map((img) => {
-            return (
-              <ImageCard key={img.id} alt={img.title} url={img.imageUrl} />
-            );
-          })}
-          <RoundIconButton iconId={'right'} onClickIconButton={handleClick} />
+        <div className={styles.challengeTitle}>
+          <h2 className={styles.title}>이번주 포토챌린지</h2>
+          <Link href={ROUTES.challenge.list} className={styles.link}>
+            더보기
+          </Link>
         </div>
-        <button className={styles.challengeAddBtn}>+ 챌린지 참여하기</button>
+        <div className={styles.challengeContainer}>
+          <div className={styles.imageCardContainer}>
+            {imageData.map((img) => {
+              return (
+                <ImageCard
+                  key={img.id}
+                  alt={img.title}
+                  url={img.imageUrl}
+                  pinId={pinIdData}
+                />
+              );
+            })}
+            <RoundIconButton iconId={'right'} onClickIconButton={handleClick} />
+          </div>
+          <Link href={'/'} className={styles.challengeAddBtn}>
+            + 챌린지 참여하기
+          </Link>
+        </div>
       </div>
     </>
   );
