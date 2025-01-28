a645ed038e1add82dc37420ec211e07cd196ea72
Son Ahyun <dkgus731@naver.com>
2025-01-28 11:59:03 +0900

    🔨 SearchInput 컴포넌트에서 값 전달받도록 수정

--- components/InputBox/SearchInput/SearchInput.tsx
+++ components/InputBox/SearchInput/SearchInput.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import React from 'react';
 import Icon from '@/components/Icon/Icon';
 import styles from './SearchInput.module.scss';
 
-const SearchInput = () => {
+interface SearchInputProps {
+  value: string; // 입력된 값
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void; // 변경 핸들러
+}
+
+const SearchInput: React.FC<SearchInputProps> = ({ value, onChange }) => {
   return (
     <div className={styles.searchWrap}>
       <div className={styles.icon}>
@@ -13,6 +19,8 @@ const SearchInput = () => {
         className={styles.searchInput}
         type='text'
         placeholder='검색어를 입력하세요.'
+        value={value} // 외부에서 전달받은 value
+        onChange={onChange} // 외부에서 전달받은 onChange 핸들러
       />
     </div>
   );
