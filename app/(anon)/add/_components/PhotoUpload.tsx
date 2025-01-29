6dafbbfc6e81e80134d8ce6893a2b367e086f508
Son Ahyun <dkgus731@naver.com>
2025-01-29 11:33:58 +0900

    🔨 Add페이지 사진 미리보기 기능 추가 및 폰트 변경

--- app/(anon)/add/_components/PhotoUpload.tsx
+++ app/(anon)/add/_components/PhotoUpload.tsx
@@ -1,13 +1,23 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../add.module.scss';
 
 const PhotoUpload: React.FC = () => {
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+
   const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (files && files[0]) {
-      console.log('Uploaded file:', files[0]);
+      const file = files[0];
+      console.log('Uploaded file:', file);
+
+      // 이미지 미리보기 설정
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
     }
   };
 
@@ -15,11 +25,19 @@ const PhotoUpload: React.FC = () => {
     <div>
       <div className={styles.photoUploadContainer}>
         <label htmlFor='photo-upload' className={styles.uploadLabel}>
-          <div className={styles.placeholder}>
-            <span className={styles.plusIcon}>+</span>
-            <span className={styles.text}>사진 추가</span>
-            <span className={styles.count}>0/1</span>
-          </div>
+          {previewImage ? (
+            <img
+              src={previewImage}
+              alt='미리보기'
+              className={styles.previewImage}
+            />
+          ) : (
+            <div className={styles.placeholder}>
+              <span className={styles.plusIcon}>+</span>
+              <span className={styles.text}>사진 추가</span>
+              <span className={styles.count}>0/1</span>
+            </div>
+          )}
           <input
             id='photo-upload'
             type='file'
