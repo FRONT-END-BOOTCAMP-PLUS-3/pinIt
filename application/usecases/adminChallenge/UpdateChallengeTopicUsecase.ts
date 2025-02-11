235df33c63a3371192884df80891b0eb025f1a86
JoostLean <jusang2@gmail.com>
2025-02-11 12:16:54 +0900

    ✨ (#81) 챌린지 주제 (추가/목록/읽기/수정) 기능 개발

---
+++ application/usecases/adminChallenge/UpdateChallengeTopicUsecase.ts
@@ -0,0 +1,25 @@
+import { ChallengeTopic } from "@/domain/entities/ChallengeTopic";
+import { ChallengeTopicRepository } from "@/domain/repositories/ChallengeTopicRepository";
+import { getUserIdFromSupabase } from "@/utils/supabase/getUserIdFromSupabase";
+
+export const updateChallengeTopicUsecase = async (
+    challengeTopicRepository: ChallengeTopicRepository,
+    data: ChallengeTopic
+): Promise<void> => {
+    // 현재 로그인된 관리자의 아이디 받아오기
+    const awaitAdminId = await getUserIdFromSupabase();
+    // adminId가 false일 경우 null로 처리
+    const adminId = awaitAdminId ? awaitAdminId : null;
+
+    // Dto를 인자로 받았고, 레포지토리에는 ChallengeTopic 엔티티의 형태로 전송해줘야 하기 때문에 변환해주기
+    // 없는건 null로 보내주면 됨
+    const newData: ChallengeTopic = {
+        id: data.id, // 기본 키 (Primary Key)
+        topic: data.topic, // 주제 제목
+        createAt: data.createAt, // 생성 날짜
+        startDate: data.startDate, // 시작 날짜
+        endDate: data.endDate, // 종료 날짜
+        adminId: adminId, // 사용자 UUID (외래 키, Foreign Key)
+    };
+    await challengeTopicRepository.updateChallengeTopic(newData);
+}
--- domain/entities/ChallengeTopic.ts
+++ domain/entities/ChallengeTopic.ts
@@ -1,7 +1,7 @@
-type UUID = string;
+type UUID = string | null;
 
 export interface ChallengeTopic {
-  id: UUID; // 기본 키 (Primary Key)
+  id: UUID | null; // 기본 키 (Primary Key)
   topic: string; // 주제 제목
   createAt: Date; // 생성 날짜
   startDate: Date; // 시작 날짜
