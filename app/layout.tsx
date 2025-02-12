d919c599f816311c28c153c03dfe9b085c9195fe
Son Ahyun <dkgus731@naver.com>
2025-02-12 15:51:56 +0900

    🔨 PWA 설정 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,10 +69,33 @@ export default function RootLayout({
     }
   };
 
+  useEffect(() => {
+    let themeColorMeta = document.querySelector("meta[name='theme-color']");
+
+    if (!themeColorMeta) {
+      themeColorMeta = document.createElement('meta');
+      themeColorMeta.setAttribute('name', 'theme-color');
+      document.head.appendChild(themeColorMeta);
+    }
+
+    const themeColor =
+      pageConfig.header === 'WhiteHeaderWithBack' ? '#FFFFFF' : '#292526';
+
+    themeColorMeta.setAttribute('content', themeColor);
+  }, [pageConfig.header]); // pageConfig.header 변경 시 실행
+
   return (
     <html lang='ko'>
       <head>
         <link rel='/manifest' href='/manifest.json' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-title' content='PinIt!' />
+        <link rel='apple-touch-icon' sizes='180x180' href='icon-192x192.png' />
+        <link
+          rel='apple-touch-startup-image'
+          media='screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+          href='icon-512x512.png'
+        />
       </head>
       <body>
         <div
