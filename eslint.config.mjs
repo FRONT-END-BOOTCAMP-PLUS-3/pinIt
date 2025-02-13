b5c6f43d52f21c036edb87925bd56ffb25885dd9
Son Ahyun <dkgus731@naver.com>
2025-02-13 15:12:20 +0900

    🔧 빌드 오류 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,14 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: ['node_modules/**', '.next/**', 'public/**'], // 무시할 경로 설정
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off', // any타입 에러 이슈 제거
-    },
-  },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
-
-export default eslintConfig;
