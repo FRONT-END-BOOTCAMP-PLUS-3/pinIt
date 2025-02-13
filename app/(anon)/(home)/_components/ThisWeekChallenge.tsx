10ee13a5f0018e76298113161df040f7b138f1eb
Son Ahyun <dkgus731@naver.com>
2025-02-13 15:43:34 +0900

    🔨 빌드 오류 수정 3

--- app/(anon)/(home)/_components/ThisWeekChallenge.tsx
+++ app/(anon)/(home)/_components/ThisWeekChallenge.tsx
@@ -1,69 +1,81 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import RoundIconButton from '@/components/Buttons/RoundIconButton';
 import styles from '../home.module.scss';
 import ImageCard from '@/components/Card/ImageCard/ImageCard';
-import Link from 'next/link';
 import ROUTES from '@/constants/routes';
 import { useEffect, useState } from 'react';
 import { ThisWeekChallengedPinListDto } from '@/application/usecases/challenge/dto/ThisWeekChallengedPinListDto';
 
 const ThisWeekChallenge = () => {
+  const router = useRouter();
   const [thisWeekChallengedPinList, setThisWeekChallengedPinList] =
     useState<ThisWeekChallengedPinListDto | null>(null);
 
   useEffect(() => {
     async function fetchThisWeekChallengedPinList() {
-      const response = await fetch('/api/fetch-this-week-challenged-pin-list');
-      if (!response.ok) {
-        console.log('이번 주 챌린지 주제가 없습니다.');
-        return null;
+      try {
+        const response = await fetch(
+          '/api/fetch-this-week-challenged-pin-list',
+        );
+        if (!response.ok) throw new Error('이번 주 챌린지 주제가 없습니다.');
+
+        const data = await response.json();
+        setThisWeekChallengedPinList(data);
+      } catch (error) {
+        console.error(error);
       }
-      const data = await response.json();
-      setThisWeekChallengedPinList(data);
     }
 
     fetchThisWeekChallengedPinList();
   }, []);
 
-  return (
-    <>
-      <div className={styles.thisWeekChallengeContainer}>
-        <div className={styles.challengeTitle}>
-          <h2 className={styles.title}>이번주 포토챌린지</h2>
-          <Link href={ROUTES.challenge.list} className={styles.link}>
-            더보기
-          </Link>
-        </div>
-        <div className={styles.challengeContainer}>
-          <div className={styles.imageCardContainer}>
-            {thisWeekChallengedPinList ? (
-              <>
-                {thisWeekChallengedPinList.pins.map((pin) => {
-                  return (
-                    <ImageCard
-                      key={pin.id}
-                      alt={pin.placeName}
-                      url={pin.image}
-                      pinId={pin.id!}
-                    />
-                  );
-                })}
-                <Link href={ROUTES.challenge.list} className={styles.link}>
-                  <RoundIconButton iconId={'right'} />
-                </Link>
-              </>
-            ) : (
-              <p>진행 중인 챌린지가 없어요.</p>
-            )}
-          </div>
+  const handleClicked = () => {
+    router.push(ROUTES.challenge.list); // 페이지 이동
+  };
 
-          <Link href={ROUTES.challenge.add} className={styles.challengeAddBtn}>
-            + 챌린지 참여하기
-          </Link>
-        </div>
+  return (
+    <div className={styles.thisWeekChallengeContainer}>
+      <div className={styles.challengeTitle}>
+        <h2 className={styles.title}>이번주 포토챌린지</h2>
+        <button onClick={handleClicked} className={styles.link}>
+          더보기
+        </button>
       </div>
-    </>
+
+      <div className={styles.challengeContainer}>
+        <div className={styles.imageCardContainer}>
+          {thisWeekChallengedPinList?.pins?.length ? (
+            <>
+              {thisWeekChallengedPinList.pins.map((pin) => (
+                <ImageCard
+                  key={pin.id}
+                  alt={pin.placeName}
+                  url={pin.image}
+                  pinId={pin.id!}
+                />
+              ))}
+              <button onClick={handleClicked} className={styles.link}>
+                <RoundIconButton
+                  iconId='right'
+                  onClickIconButton={handleClicked}
+                />
+              </button>
+            </>
+          ) : (
+            <p>진행 중인 챌린지가 없어요.</p>
+          )}
+        </div>
+
+        <button
+          onClick={() => router.push(ROUTES.challenge.add)}
+          className={styles.challengeAddBtn}
+        >
+          + 챌린지 참여하기
+        </button>
+      </div>
+    </div>
   );
 };
 
